84b4e7146f0e4cd179bcdbf41e0238c3ec405e64
jei3m <virozzegt@gmail.com>
2024-12-18 11:21:32 +0800

    Updated function for localStorage.getItem as a useState also implemented routes for Riders page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+// App.jsx
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/OrderDetails';
 import './App.css';
@@ -8,34 +9,31 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import PrivateRoute from './components/PrivateRoute';
 import Header from './components/Header';
+import Riders from './pages/Riders';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem('isAuthenticated') === 'true' ? true : false);
 
-  useEffect(() => {
-    const storedAuth = localStorage.getItem('isAuthenticated');
-    if (storedAuth === 'true') {
-      setIsAuthenticated(true);
-    }
-  });
 
   const handleLogout = () => {
     setIsAuthenticated(false);
-    localStorage.removeItem('isAuthenticated', 'true');
-    window.location.reload();
+    localStorage.removeItem('isAuthenticated');
   };
 
   return (
     <Router>
-      {isAuthenticated && <Header handleLogout={handleLogout} />} {/* Only show header when authenticated */}
+      {isAuthenticated && <Header handleLogout={handleLogout} />}
       <Routes>
-        <Route path="/" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-        <Route path="/signup" element={<Signup/>}/>
-        
+        <Route path="/" element={isAuthenticated ? <Navigate to="/orders" /> : <Login setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/signup" element={<Signup />} />
+
         <Route element={<PrivateRoute isAuthenticated={isAuthenticated} />}>
           <Route path="/orders/:orderId" element={<Dashboard />} />
           <Route path="/orders" element={<Orders />} />
+          <Route path="/riders" element={<Riders />} />
+          <Route path="/riders/:riderName" element={<Riders />} />
         </Route>
+        
       </Routes>
     </Router>
   );
