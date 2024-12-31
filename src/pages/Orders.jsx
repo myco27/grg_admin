ede94f81b650f931faaae53e9093f058b3201ed0
DonMonsieur <124369678+DonMonsieur@users.noreply.github.com>
2024-12-31 13:48:58 +0800

    # add loading onstatuschange

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -111,7 +111,7 @@ export default function Orders() {
   const handleClickStatus = (status) => {
     const updatedStatus = status === "all" ? "" : status;
     setStatus(updatedStatus);
-    setPagination({ ...pagination, page: 1 });
+    setPagination({ ...pagination, page: 1, isLoading: true });
 
     if (updatedStatus) {
       newSearchParams.set("status", updatedStatus);
@@ -138,6 +138,7 @@ export default function Orders() {
     const { value } = event.target;
 
     setFilters({ ...filters, search: value });
+    // setPagination({ ...pagination, page: 1, isLoading: true });
     setSearchLoading(true);
 
     newSearchParams.set("search", value);
@@ -165,48 +166,45 @@ export default function Orders() {
   return (
     <Fragment>
       <div className="flex flex-col min-h-screen bg-gray-100">
-        {pagination.isLoading ? (
-          <Loading />
-        ) : (
-          <main className="flex-1 px-3 md:px-8 py-4">
-            {/* Header and Controls */}
-            <div className="flex flex-col gap-4 mb-6">
-              <Typography variant="h4" className="text-gray-900">
-                Transaction
-              </Typography>
+        <main className="flex-1 px-3 md:px-8 py-4">
+          {/* Header and Controls */}
+          <div className="flex flex-col gap-4 mb-6">
+            <Typography variant="h4" className="text-gray-900">
+              Transaction
+            </Typography>
 
-              <div className=" relative flex flex-col md:flex-row items-center justify-between gap-4">
-                {/* ORDERS STATUS */}
-                <Tabs
-                  value={status || "all"}
-                  className="w-full md:w-fit border px-1 border-gray-400 py-0.5 bg-white rounded-lg relative overflow-x-auto xl:overflow-visible"
-                >
-                  <div>
-                    <TabsHeader
-                      className="bg-transparent gap-x-4"
-                      indicatorProps={{
-                        className: `bg-purple-200 shadow-none ${
-                          status === "pending"
-                            ? "bg-yellow-200 text-yellow-900"
-                            : status === "processing"
-                            ? "bg-blue-200 text-blue-900"
-                            : status === "intransit"
-                            ? "bg-gray-200 text-gray-900"
-                            : status === "cancelled"
-                            ? "bg-red-200 text-red-900"
-                            : status === "completed"
-                            ? "bg-purple-200 text-purple-900"
-                            : status === "all"
-                            ? ""
-                            : "bg-gray-200 text-gray-900"
-                        }`,
-                      }}
-                    >
-                      {countOrders.map(({ status, count }) => (
-                        <Badge
-                          key={status}
-                          content={count}
-                          className={`absolute top-1 z-50 text-[8px] min-w-[14px] min-h-[14px] px-[6px] py-[2px] 
+            <div className=" relative flex flex-col md:flex-row items-center justify-between gap-4">
+              {/* ORDERS STATUS */}
+              <Tabs
+                value={status || "all"}
+                className="w-full md:w-fit border px-1 border-gray-400 py-0.5 bg-white rounded-lg relative overflow-x-auto xl:overflow-visible"
+              >
+                <div>
+                  <TabsHeader
+                    className="bg-transparent gap-x-4"
+                    indicatorProps={{
+                      className: `bg-purple-200 shadow-none ${
+                        status === "pending"
+                          ? "bg-yellow-200 text-yellow-900"
+                          : status === "processing"
+                          ? "bg-blue-200 text-blue-900"
+                          : status === "intransit"
+                          ? "bg-gray-200 text-gray-900"
+                          : status === "cancelled"
+                          ? "bg-red-200 text-red-900"
+                          : status === "completed"
+                          ? "bg-purple-200 text-purple-900"
+                          : status === "all"
+                          ? ""
+                          : "bg-gray-200 text-gray-900"
+                      }`,
+                    }}
+                  >
+                    {countOrders.map(({ status, count }) => (
+                      <Badge
+                        key={status}
+                        content={count}
+                        className={`absolute top-1 z-50 text-[8px] min-w-[14px] min-h-[14px] px-[6px] py-[2px] 
                           ${
                             status === "cancelled"
                               ? "bg-red-200 text-red-900"
@@ -222,52 +220,55 @@ export default function Orders() {
                               ? ""
                               : "bg-gray-200 text-gray-900"
                           }`}
+                      >
+                        <Tab
+                          key={status}
+                          value={status}
+                          onClick={() => handleClickStatus(status)}
+                          className="text-sm font-medium text-gray-800"
                         >
-                          <Tab
-                            key={status}
-                            value={status}
-                            onClick={() => handleClickStatus(status)}
-                            className="text-sm font-medium text-gray-800"
-                          >
-                            {status.charAt(0).toUpperCase() + status.slice(1)}
-                          </Tab>
-                        </Badge>
-                      ))}
-                    </TabsHeader>
-                  </div>
-                </Tabs>
+                          {status.charAt(0).toUpperCase() + status.slice(1)}
+                        </Tab>
+                      </Badge>
+                    ))}
+                  </TabsHeader>
+                </div>
+              </Tabs>
 
-                <div className="flex flex-col sm:flex-row gap-3 w-full md:w-auto">
-                  {/* SEARCH FILTER */}
-                  <div className="w-full sm:w-72">
-                    <Input
-                      label="Search orders..."
-                      icon={
-                        searchLoading ? (
-                          <Spinner className="h-5 w-5" />
-                        ) : (
-                          <Search className="h-5 w-5" />
-                        )
-                      }
-                      size="md"
-                      className="bg-white"
-                      value={filters.search}
-                      onChange={(e) => handleSearchInput(e)}
-                    />
-                  </div>
-
-                  {/* DATE FILTER */}
-                  <DatePicker
-                    selected={filters.date}
-                    onChange={(e) => handleDateChange(e)}
-                    placeholder="Filter by date"
-                    isClearable
+              <div className="flex flex-col sm:flex-row gap-3 w-full md:w-auto">
+                {/* SEARCH FILTER */}
+                <div className="w-full sm:w-72">
+                  <Input
+                    label="Search orders..."
+                    icon={
+                      searchLoading ? (
+                        <Spinner className="h-5 w-5" />
+                      ) : (
+                        <Search className="h-5 w-5" />
+                      )
+                    }
+                    size="md"
+                    className="bg-white"
+                    value={filters.search}
+                    onChange={(e) => handleSearchInput(e)}
                   />
                 </div>
+
+                {/* DATE FILTER */}
+                <DatePicker
+                  selected={filters.date}
+                  onChange={(e) => handleDateChange(e)}
+                  placeholder="Filter by date"
+                  isClearable
+                />
               </div>
             </div>
+          </div>
 
-            {/* Orders Grid */}
+          {/* Orders Grid */}
+          {pagination.isLoading ? (
+            <Loading />
+          ) : (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {orders.length > 0 ? (
                 orders.map((order) => (
@@ -283,19 +284,18 @@ export default function Orders() {
                 </div>
               )}
             </div>
-
-            {/* Pagination */}
-            <Pagination
-              currentPage={pagination.page}
-              totalItems={pagination.totalItems}
-              itemsPerPage={pagination.itemsPerPage}
-              totalPages={pagination.totalPages}
-              onPageChange={(newPage) => handlePageChange(newPage)}
-              isLoading={pagination.isLoading}
-              links={pagination.links}
-            />
-          </main>
-        )}
+          )}
+          {/* Pagination */}
+          <Pagination
+            currentPage={pagination.page}
+            totalItems={pagination.totalItems}
+            itemsPerPage={pagination.itemsPerPage}
+            totalPages={pagination.totalPages}
+            onPageChange={(newPage) => handlePageChange(newPage)}
+            isLoading={pagination.isLoading}
+            links={pagination.links}
+          />
+        </main>
       </div>
     </Fragment>
   );
