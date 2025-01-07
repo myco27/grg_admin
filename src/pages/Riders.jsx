7f95177e03cc019040e6d6519d47bfb7fdc2f821
jei3m <virozzegt@gmail.com>
2025-01-07 13:09:48 +0800

    Pagination for Rider list section, added debounce for search, fetch rider orders

--- src/pages/Riders.jsx
+++ src/pages/Riders.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect, Fragment, useCallback } from 'react';
-import { Typography, Input, Button, Spinner } from "@material-tailwind/react";
+import { Typography, Input, Button, Spinner, IconButton } from "@material-tailwind/react";
 import RiderCard from '../components/RidersPage/RiderCard';
 import DetailsCard from '../components/RidersPage/OrderDetailsCard';
-import RiderDetails from '../components/RidersPage/RiderDetailsCard'; // Import RiderDetails component
+import Pagination from '../components/RidersPage/Pagination';
+import RiderDetails from '../components/RidersPage/RiderDetailsCard';
 import MapCard from '../components/RidersPage/MapCard';
+import Loading from "../components/layout/Loading";
 import { useParams, useNavigate, useSearchParams } from 'react-router-dom';
-import { Search, ChevronUp, ChevronDown } from "lucide-react";
+import { Search, ChevronUp, ChevronDown, ChevronRight, ChevronLeft } from "lucide-react";
+import useDebounce from "../components/UseDebounce";
 import axiosClient from '../axiosClient';
 
 export default function Riders() {
-  const { riderId } = useParams(); // Changed from riderName to riderId
+  const { riderId } = useParams();
   const navigate = useNavigate();
   const [selectedRider, setSelectedRider] = useState(null);
   const [riders, setRiders] = useState([]);
@@ -17,108 +20,130 @@ export default function Riders() {
   const [filteredRiders, setFilteredRiders] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
   const [cache, setCache] = useState({});
-  // const [loading, setLoading] = useState(true); // Add loading state
-  const [error, setError] = useState(null); // Add error state
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [searchLoading, setSearchLoading] = useState(true);
 
-  const fetchRiders = useCallback(
-    async () => {
-      // Create cache key
-      const cacheKey = 'riders';
-  
-      // Check cache
-      if (cache[cacheKey]) {
-        setRiders(cache[cacheKey].data);
-        // setLoading(false);
-        return;
+  // Pagination state
+  const [currentPage, setCurrentPage] = useState(1);
+  const [ridersPerPage] = useState(8); // Number of riders per page
+
+  // Debouncing
+  const debouncedSearchQuery = useDebounce({ value: searchQuery });
+
+  const fetchRiders = useCallback(async () => {
+    const cacheKey = 'riders';
+
+    if (cache[cacheKey]) {
+      setRiders(cache[cacheKey].data);
+      setIsLoading(false);
+      setSearchLoading(false);
+      return;
+    }
+    try {
+      const response = await axiosClient.get('/admin/riders');
+      setSearchLoading(true);
+
+      if (response.status === 200) {
+        const ridersData = response.data.data;
+        setRiders(ridersData);
+        setIsLoading(false);
+
+        setCache((prevCache) => ({
+          ...prevCache,
+          [cacheKey]: { data: ridersData },
+        }));
       }
-  
-      try {
-        // setLoading(true);
-        const response = await axiosClient.get('/admin/riders');
-        
-        if (response.status === 200) {
-          const ridersData = response.data.data;
-  
-          // Update state
-          setRiders(ridersData);
-          // setLoading(false);
-  
-          // Cache the data
-          setCache((prevCache) => ({
-            ...prevCache,
-            [cacheKey]: { data: ridersData },
-          }));
-        }
-      } catch (error) {
-        setError('Failed to fetch riders');
-        // setLoading(false);
-      }
-    },
-    [cache], 
-  );
+    } catch (error) {
+      setError('Failed to fetch riders');
+      setIsLoading(false);
+      setSearchLoading(false);
+    }
+  }, [cache]);
+
+  const fetchRiderOrder = async (riderId) => {
+    try {
+      const response = await axiosClient.get(`/admin/riders/order`,
+        { params: { id: 15 } }
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   useEffect(() => {
     fetchRiders();
-  }, [fetchRiders]);
+    fetchRiderOrder();
+  }, [fetchRiders, debouncedSearchQuery]);
 
   useEffect(() => {
-    const urlSearchQuery = searchParams.get('search') || '';
-    setSearchQuery(urlSearchQuery);
-
-    // Filter riders based on search query
     const filtered = riders.filter(rider => {
-      const riderFullName = `${rider.first_name} ${rider.last_name}` || ''; // Combine first and last name for search
-      return riderFullName.toLowerCase().includes(urlSearchQuery.toLowerCase());
+      const riderFullName = `${rider.first_name} ${rider.last_name}` || '';
+      return riderFullName.toLowerCase().includes(debouncedSearchQuery.toLowerCase());
     });
     setFilteredRiders(filtered);
-
-    // Set the selected rider based on the URL or default to the first rider
+  
     if (riderId) {
-      const foundRider = riders.find(rider => rider.id === parseInt(riderId)); // Match rider by ID
+      const foundRider = riders.find(rider => rider.id === parseInt(riderId));
       setSelectedRider(foundRider || (riders.length > 0 ? riders[0] : null));
     } else if (riders.length > 0) {
       setSelectedRider(riders[0]);
     }
-  }, [riderId, searchParams, riders]);
+  }, [riderId, debouncedSearchQuery, riders]); 
 
   const handleRiderSelect = (rider) => {
     setSelectedRider(rider);
-    navigate(`/riders/${rider.id}?search=${searchQuery}`); // Updated to use rider ID
+    navigate(`/riders/${rider.id}?search=${searchQuery}`);
   };
 
   const handleSearchChange = (e) => {
     const query = e.target.value;
     setSearchQuery(query);
     setSearchParams({ search: query });
+    setCurrentPage(1); // Reset to the first page when searching
   };
+
+  // Pagination logic
+  const indexOfLastRider = currentPage * ridersPerPage;
+  const indexOfFirstRider = indexOfLastRider - ridersPerPage;
+  const currentRiders = filteredRiders.slice(indexOfFirstRider, indexOfLastRider);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <Fragment>
       <div className='bg-gray-100 w-full'>
-        <div className="mx-auto p-4">
+        <div className="mx-auto">
           <div className="flex flex-col lg:flex-row gap-4">
 
             {/* Left side - Rider List */}
-            <div className="h-[89dvh] overflow-y-auto lg:w-[340px] bg-white rounded-lg border border-gray-300 py-4">
+            <div className="h-auto lg:h-[89dvh] lg:w-[340px] bg-white rounded-lg border border-gray-300 py-4">
               <Typography variant="h5" className="font-semibold text-black ml-4">All Riders</Typography>
               <div className="w-full p-2 mb-2">
                 <Input
                   label="Search riders..."
-                  icon={<Search className="h-5 w-5" />}
+                  icon={
+                    searchLoading || isLoading ? (
+                      <Spinner className="h-5 w-5" />
+                    ) : (
+                      <Search className="h-5 w-5" />
+                    )
+                  }
                   size="md"
                   className="bg-white"
                   value={searchQuery}
                   onChange={handleSearchChange}
                 />
               </div>
-              { filteredRiders.length === 0 ? (
+              {currentRiders.length === 0 ? (
                 <div className="text-center py-8">
                   <Typography color="gray" className="font-medium">
                     No riders available.
                   </Typography>
                 </div>
               ) : (
-                filteredRiders.map((rider) => (
+                currentRiders.map((rider) => (
                   <div
                     key={rider.id}
                     onClick={() => handleRiderSelect(rider)}
@@ -128,20 +153,36 @@ export default function Riders() {
                   </div>
                 ))
               )}
+
+              {/* Pagination Controls */}
+              <Pagination
+                currentPage={currentPage}
+                paginate={paginate}
+                indexOfLastRider={indexOfLastRider}
+                filteredRiders={filteredRiders}
+                ridersPerPage={ridersPerPage}
+              />
+
             </div>
 
             {/* Right side - Rider Details */}
-            <div className="w-full">
-              {selectedRider ? (
-                <div className="h-[89dvh] overflow-y-auto bg-white rounded-lg border border-gray-300 p-4">
-                  <Typography variant="h5" className="text-black mb-4">Rider Details</Typography>
-                  <RiderDetails rider={selectedRider} />
+            <div className="w-full bg-white h-[89dvh] overflow-y-auto rounded-lg border border-gray-300 p-4">
+              {isLoading ? (
+                <div className="flex items-center justify-center overflow-y-hidden">
+                  <Loading />
                 </div>
               ) : (
-                <div className="text-center py-8">
-                  <Typography color="gray" className="font-medium">
-                    Select a rider to view details.
-                  </Typography>
+                <div className="">
+                  <Typography variant="h5" className="text-black mb-4">Rider Details</Typography>
+                  {selectedRider ? (
+                    <RiderDetails rider={selectedRider} />
+                  ) : (
+                    <div className="text-center py-8">
+                      <Typography color="gray" className="font-medium">
+                        No rider selected.
+                      </Typography>
+                    </div>
+                  )}
                 </div>
               )}
             </div>
