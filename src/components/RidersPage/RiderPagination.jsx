a20e663cd81b53e0b8791f8f3f618082284c64d6
jei3m <virozzegt@gmail.com>
2025-01-08 14:21:35 +0800

    support server side pagination

--- src/components/RidersPage/RiderPagination.jsx
+++ src/components/RidersPage/RiderPagination.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import { Button, IconButton } from "@material-tailwind/react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
-export default function Pagination({ currentPage, paginate, indexOfLastRider, filteredRiders, ridersPerPage }) {
+export default function Pagination({ currentPage, paginate, totalRiders, ridersPerPage }) {
   const isFirstPage = currentPage === 1;
-  const isLastPage = indexOfLastRider >= filteredRiders.length;
-  const lastPage = Math.ceil(filteredRiders.length / ridersPerPage);
+  const isLastPage = currentPage * ridersPerPage >= totalRiders;
+  const lastPage = Math.ceil(totalRiders / ridersPerPage);
 
   return (
     <div className="flex justify-center mt-2 gap-1">
       {/* First Page Button */}
       <Button
         color="purple"
-        size="sm" // Smaller button size
+        size="sm"
         onClick={() => paginate(1)}
         disabled={isFirstPage}
-        className="px-2 py-1 text-xs normal-case" // Smaller padding and text
+        className="px-2 py-1 text-xs normal-case"
       >
         First
       </Button>
@@ -23,29 +23,29 @@ export default function Pagination({ currentPage, paginate, indexOfLastRider, fi
       {/* Previous Page Button */}
       <IconButton
         color="purple"
-        size="sm" // Smaller button size
+        size="sm"
         onClick={() => paginate(currentPage - 1)}
         disabled={isFirstPage}
-        className="p-1" // Smaller padding
+        className="p-1"
       >
-        <ChevronLeft className="h-4 w-4" /> {/* Smaller icon */}
+        <ChevronLeft className="h-4 w-4" />
       </IconButton>
 
       {/* Next Page Button */}
       <IconButton
         color="purple"
-        size="sm" // Smaller button size
+        size="sm"
         onClick={() => paginate(currentPage + 1)}
         disabled={isLastPage}
-        className="p-1" // Smaller padding
+        className="p-1"
       >
-        <ChevronRight className="h-4 w-4" /> {/* Smaller icon */}
+        <ChevronRight className="h-4 w-4" />
       </IconButton>
 
       {/* Last Page Button */}
       <Button
         color="purple"
-        size="sm" // Smaller button size
+        size="sm"
         onClick={() => paginate(lastPage)}
         disabled={isLastPage}
         className="px-2 py-1 text-xs normal-case"
