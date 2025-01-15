f4322ef38c4f0232b6b836f898d227a21efafd49
jei3m <virozzegt@gmail.com>
2025-01-15 09:46:27 +0800

    style: responsiveness of sub text

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,12 +7,12 @@ import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 function NotFound() {
 
   return (
-    <div className="flex flex-col mx-auto items-center justify-center h-screen min-w-screen bg-gray-100">
+    <div className="flex flex-col mx-auto items-center justify-center h-screen min-w-screen bg-gray-100 p-4 md:p-0">
       <ExclamationTriangleIcon className="h-20 w-20 text-red-500 mb-4" />
       <Typography variant="h1" color="red" className="mb-4">
         Error 404
       </Typography>
-      <Typography variant="h5" color="gray" className="mb-8">
+      <Typography color="gray" className="mb-8 text-center text-xl md:text-2xl">
         Oops! The page you're looking for could not be found.
       </Typography>
 
