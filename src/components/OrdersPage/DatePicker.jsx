f76787748725ad9ea56d185a7e3c9e040dac4e46
jei3m <virozzegt@gmail.com>
2025-01-17 11:25:44 +0800

    style: Adjust input height in DatePicker component for consistency

--- src/components/OrdersPage/DatePicker.jsx
+++ src/components/OrdersPage/DatePicker.jsx
@@ -53,7 +53,7 @@ export default function DatePicker({
       <div className="relative">
         <input
           ref={inputRef}
-          className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+          className="peer h-10 w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
           placeholder=" "
           value={isValidDate ? format(new Date(selected), "PPP") : ""} // Only format valid dates
           readOnly
