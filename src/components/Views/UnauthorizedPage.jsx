eff08ee5c91e9550c1d2c722339721974a759cdd
DonMonsieur <124369678+DonMonsieur@users.noreply.github.com>
2025-03-13 18:18:37 +0800

    # modify unauthorize page

--- src/components/Views/UnauthorizedPage.jsx
+++ src/components/Views/UnauthorizedPage.jsx
@@ -1,27 +1,24 @@
-// src/pages/UnauthorizedPage.jsx
 import React from "react";
 import { Button, Typography } from "@material-tailwind/react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { NoSymbolIcon } from "@heroicons/react/24/outline";
 
 function UnauthorizedPage() {
+  const navigate = useNavigate();
 
   return (
-    <div className="min-w-screen mx-auto flex h-screen flex-col items-center justify-center bg-gray-100 p-4 md:p-0">
+    <div className="mx-auto flex h-[80dvh] flex-col items-center justify-center bg-gray-100 p-4 md:p-0">
       <NoSymbolIcon className="mb-4 h-20 w-20 text-red-500" />
       <Typography variant="h1" color="red" className="mb-4">
         Error 401
       </Typography>
       <Typography color="gray" className="mb-8 text-center text-xl md:text-2xl">
-        NO AUTHORIZATION FOUND!
+        You are not authorized to access this page.
       </Typography>
 
-      <Link to="/user-management">
-        <Button variant="filled" color="purple">
-          Go HOME
-        </Button>
-      </Link>
-
+      <Button variant="filled" color="purple" onClick={() => navigate(-1)}>
+        Go Back
+      </Button>
     </div>
   );
 }
