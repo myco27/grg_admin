cfd8545c37f5654d587e460acea10b3f8d54ee8f
DanezseWork <dpjose.work@gmail.com>
2025-03-24 13:24:05 +0800

    password val
    password validation 1

--- src/components/Profile/ProfileModal.jsx
+++ src/components/Profile/ProfileModal.jsx
@@ -12,8 +12,10 @@ import { UserRoundCog, LockKeyhole } from 'lucide-react';
 import { Base, Header, Body, Footer, Sidebar } from "../Modal";
 import { useStateContext } from "../../contexts/contextProvider";
 
-const ProfileModal = ({ open, handleOpen, userId, userType, fetchUsers }) => {
 
+const ProfileModal = ({ open, handleOpen, userId, userType, fetchUsers }) => {
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
   const [password, setPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -26,6 +28,33 @@ const ProfileModal = ({ open, handleOpen, userId, userType, fetchUsers }) => {
   const { showAlert } = useAlert();
   const [activeTab, setActiveTab] = useState("basic_setting");
   const { user } = useStateContext();
+
+  const changePassword = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axiosClient.post(`/admin/users/update-password/${userId}`, {
+        old_password: password,
+        new_password: newPassword,
+        new_password_confirmation: confirmPassword,
+      });
+      if (response.status === 200) {
+        showAlert("Password updated successfully!", "success");
+        handleOpen();
+      }
+    } catch (error) {
+      if (error.response?.data?.validation_error) {
+        Object.values(error.response.data.validation_error)
+          .flat()
+          .forEach((errorMessage) => showAlert(errorMessage, "error"));
+      } else if (error.response?.data?.error_message == "Old password is incorrect!") {
+        setPasswordError(true); // Set the error state to true
+        setPasswordErrorMessage(error.response?.data?.error_message); // error message
+      } else {
+        showAlert("An error occurred. Please try again.", "error");
+      }
+      
+    }
+  }
 
   // const fetchUserDetails = async () => {
   //   try {
@@ -89,10 +118,16 @@ const ProfileModal = ({ open, handleOpen, userId, userType, fetchUsers }) => {
   };
 
   const handleSubmit = (event) => {
-    event.preventDefault();
-    setLoading(true);
-    updateUser();
-  };
+  event.preventDefault();
+  setLoading(true);
+  if (activeTab === "security") {
+    changePassword(event);
+  } else {
+    updateUser();  // Only if updating user details
+  }
+};
+
+
 
   // useEffect(() => {
   //   setLoading(true);
@@ -175,78 +210,90 @@ const ProfileModal = ({ open, handleOpen, userId, userType, fetchUsers }) => {
       value: "security",
       label: "Security",
       icon: <LockKeyhole />,
+      handleSubmit: changePassword,
       content: (
         <>
-          <div className="relative py-2">
-            <Input
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              label="Current Password"
-              type={passwordVisibility.password ? "text" : "password"}
-              className="pr-10"
-              autoComplete="new-password"
-            />
-            <button
-              type="button"
-              className="absolute right-3 top-4 text-gray-500 hover:text-gray-700"
-              onClick={() => toggleVisibility("password")}
-            >
-              {passwordVisibility.password ? (
-                <EyeSlashIcon className="h-5 w-5" />
-              ) : (
-                <EyeIcon className="h-5 w-5" />
-              )}
-            </button>
-          </div>
-
-          <div className="py-2">
-            <div className="relative">
+          <form>
+            <div className="relative py-2">
               <Input
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-                label="New Password"
-                type={passwordVisibility.newPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                  setPasswordError(false); // Reset error on change
+                  setPasswordErrorMessage("");
+                }}
+                label="Current Password"
+                type={passwordVisibility.password ? "text" : "password"}
+                id="current-password"
+                error={passwordError}
                 className="pr-10"
                 autoComplete="new-password"
               />
               <button
                 type="button"
-                className="absolute right-3 top-2 text-gray-500 hover:text-gray-700"
-                onClick={() => toggleVisibility("newPassword")}
+                className="absolute right-3 top-4 text-gray-500 hover:text-gray-700"
+                onClick={() => toggleVisibility("password")}
               >
-                {passwordVisibility.newPassword ? (
+                {passwordVisibility.password ? (
                   <EyeSlashIcon className="h-5 w-5" />
                 ) : (
                   <EyeIcon className="h-5 w-5" />
                 )}
               </button>
             </div>
-            <Typography variant="small" className="text-xs text-gray-500 pt-2">
-              Your password must be at least 8 characters and should include a combination of numbers, letters and special characters (!$@%).
-            </Typography>
-          </div>
+            {passwordError && (
+              <Typography id="current-password-error" variant="small" className="text-xs text-red-500 font-semibold pb-2">{passwordErrorMessage}</Typography>
+            )}
 
-          <div className="relative py-2">
-            <Input
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              label="Confirm Password"
-              type={passwordVisibility.confirmPassword ? "text" : "password"}
-              className="pr-10"
-              autoComplete="new-password"
-            />
-            <button
-              type="button"
-              className="absolute right-3 top-4 text-gray-500 hover:text-gray-700"
-              onClick={() => toggleVisibility("confirmPassword")}
-            >
-              {passwordVisibility.confirmPassword ? (
-                <EyeSlashIcon className="h-5 w-5" />
-              ) : (
-                <EyeIcon className="h-5 w-5" />
-              )}
-            </button>
-          </div>
+            <div className="py-2">
+              <div className="relative">
+                <Input
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                  label="New Password"
+                  type={passwordVisibility.newPassword ? "text" : "password"}
+                  className="pr-10"
+                  autoComplete="new-password"
+                />
+                <button
+                  type="button"
+                  className="absolute right-3 top-2 text-gray-500 hover:text-gray-700"
+                  onClick={() => toggleVisibility("newPassword")}
+                >
+                  {passwordVisibility.newPassword ? (
+                    <EyeSlashIcon className="h-5 w-5" />
+                  ) : (
+                    <EyeIcon className="h-5 w-5" />
+                  )}
+                </button>
+              </div>
+              <Typography variant="small" className="text-xs text-gray-500 pt-2">
+                Your password must be at least 8 characters and should include a combination of numbers, letters and special characters (!$@%).
+              </Typography>
+            </div>
+
+            <div className="relative py-2">
+              <Input
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                label="Confirm Password"
+                type={passwordVisibility.confirmPassword ? "text" : "password"}
+                className="pr-10"
+                autoComplete="new-password"
+              />
+              <button
+                type="button"
+                className="absolute right-3 top-4 text-gray-500 hover:text-gray-700"
+                onClick={() => toggleVisibility("confirmPassword")}
+              >
+                {passwordVisibility.confirmPassword ? (
+                  <EyeSlashIcon className="h-5 w-5" />
+                ) : (
+                  <EyeIcon className="h-5 w-5" />
+                )}
+              </button>
+            </div>
+          </form>
         </>
       )
     }
@@ -269,7 +316,7 @@ const ProfileModal = ({ open, handleOpen, userId, userType, fetchUsers }) => {
               <Footer 
                 loading={loading} 
                 onCancel={handleOpen} 
-                onSubmit={handleSubmit}
+                onSubmit={tabs.find(tab => tab.value === activeTab)?.handleSubmit}
               />
             </div>
           </div>
