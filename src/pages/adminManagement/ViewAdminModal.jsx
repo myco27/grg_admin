750e4298ea0ad9bfe8b6265c9e4ed76965692415
Xyrone Sanchez <sanchezxyronejei@gmail.com>
2025-03-25 13:33:43 +0800

    Removed: Remove previous format.

--- src/pages/adminManagement/ViewAdminModal.jsx
+++ src/pages/adminManagement/ViewAdminModal.jsx
@@ -169,66 +169,7 @@ content: (
     </>
 
   )
-    {/*
-    <Dialog
-      aria-modal="true"
-      open={viewOpen}
-      handler={viewHandleOpen}
-      className="min-h-fit w-32 overflow-auto"
-    >
-      <DialogHeader>User Details</DialogHeader>
-      <DialogBody className="items-center justify-center">
-        {loading ? (
-          <div className="flex h-[50vh] items-center justify-center">
-            <div className="mt-[-10vh] h-16 w-16 animate-spin rounded-full border-8 border-gray-300 border-t-purple-500" />
-          </div>
-        ) : (
-          <div className="flex h-full w-full flex-col gap-5">
-            <div className="flex flex-row gap-x-20">
-              <div>
-                <Typography variant="h5">First Name</Typography>
-                <Typography>{formData.first_name}</Typography>
-              </div>
-              <div>
-                <Typography variant="h5">Last Name</Typography>
-                <Typography>{formData.last_name}</Typography>
-              </div>
-            </div>
-            <div>
-              <Typography variant="h5">Email</Typography>
-              <Typography>{formData.email}</Typography>
-            </div>
-            <div className="flex flex-wrap">
-              <Typography variant="h5">Roles:</Typography>
-              <Chip
-                className="m-1 max-w-fit bg-purple-50 text-purple-900"
-                color="purple"
-                value={formData.role}
-              ></Chip>
-            </div>
-            <div className="flex flex-wrap overflow-auto">
-              <Typography variant="h5">Permissions: </Typography>
-              {Object.entries(formData.permissions).map(([index, value]) => (
-                <div key={index}>
-                  <Chip
-                    color="purple"
-                    className="m-1 max-w-fit bg-purple-50 text-purple-900"
-                    value={value}
-                  ></Chip>
-                </div>
-              ))}
-            </div>
-          </div>
-        )}
-      </DialogBody>
-      <DialogFooter>
-        <Button variant="gradient" color="gray" onClick={viewHandleOpen}>
-          <span>Cancel</span>
-        </Button>
-      </DialogFooter>
-    </Dialog>
-    */}
-
+    
   
 };
 
