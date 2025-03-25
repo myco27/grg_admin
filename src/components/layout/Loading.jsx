0814e377f01efa526b21f0ed2dd797b16c441807
Xyrone Sanchez <sanchezxyronejei@gmail.com>
2025-03-25 15:33:45 +0800

    #Fixed: Loading Component Overflowing and responsiveness in view admin modal

--- src/components/layout/Loading.jsx
+++ src/components/layout/Loading.jsx
@@ -3,13 +3,9 @@ import React from 'react';
 
 function Loading() {
   return (
-    <div
-      className="flex justify-center items-center h-screen w-screen"
-    >
-      <div
-        className="border-8 border-gray-300 border-t-purple-500 rounded-full w-16 h-16 animate-spin mt-[-10vh]"
-      />
-    </div>
+<div className="flex h-[50vh] items-center justify-center">
+        <div className="mt-[-10vh] h-16 w-16 animate-spin rounded-full border-8 border-gray-300 border-t-purple-500" />
+      </div>
   );
 }
 
