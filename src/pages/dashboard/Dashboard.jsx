7d40b1c5c4efcac51e9c20dcdea93d05c326e10b
Xyrone Sanchez <sanchezxyronejei@gmail.com>
2025-04-04 09:58:46 +0800

    dynamic trending up icon update

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -9,17 +9,36 @@ import axiosClient from "../../axiosClient";
 import OrderRate from "../../components/Charts/OrderRate";
 import SalesByRestaurant from "../../components/Charts/SalesByRestaurant";
 import SalesByCentral from "../../components/Charts/SalesByCentral";
-import { Bike, Building2, Hotel, UserRound } from "lucide-react";
+import { Bike, Building2, Hotel, TrendingDown, TrendingUp, UserRound } from "lucide-react";
 
 const Dashboard = () => {
   const [activeCustomers, setActiveCustomers] = useState(0);
   const [activeRiders, setActiveRiders] = useState(0);
   const [totalCentral, setTotalCentral] = useState(0);
   const [totalRestaurants, setTotalRestaurants] = useState(0);
-
+  const [customerTrend, setCustomerTrend] = useState(true)
+  const [riderTrend, setRiderTrend] = useState(false)
   const [loading, setLoading] = useState(false);
 
 
+
+  const fetchStoreMontly = async () => {
+    try{
+      setLoading(true)
+      const response = await axiosClient.get("/admin/dashboard/card-data");
+      
+      if (response=== 200){
+        
+      }
+      
+    }catch(e){console.log(e.error)}
+    finally{setLoading(false)}
+
+    
+  }
+
+
+  
   const fetchDashboardCardData = async () => {
     setLoading(true);
     try {
@@ -31,6 +50,8 @@ const Dashboard = () => {
         setTotalCentral(response.data.data.totalNumberOfCentral);
         setTotalRestaurants(response.data.data.totalNumberOfRestaurant);
       }
+
+      console.log(response.data)
     } catch (error) {
       console.error("Error fetching dashboard card data:", error);
     } finally {
@@ -38,11 +59,20 @@ const Dashboard = () => {
     }
   };
 
+
+
+
+ 
+  
   useEffect(() => {
     fetchDashboardCardData();
   }, []);
 
+
+  
+
   return (
+    <Card className="rounded-none shadow-none">
     <div className="m-5 flex flex-col justify-center gap-5">
       <div id="row1" className="flex flex-grow flex-col justify-evenly gap-1 sm:flex-row">
         <Card className="item-center flex max-w-[370px] justify-center">
@@ -74,9 +104,12 @@ const Dashboard = () => {
                   <div className="h-10 w-10 animate-spin rounded-full border-t-purple-500" />
                 </div>
               ) : (
-                <Typography variant="h3" className="text-primary">
+                <div className="flex flex-row items-center justify-center gap-2">
+                <Typography variant="h3" className={`${customerTrend?"text-primary":"text-[#C70381]"}`}>
                   {activeCustomers}
                 </Typography>
+                {customerTrend?<TrendingUp className="text-primary"/>:<TrendingDown className="text-[#C70381]"/>}
+                </div>
               )}
             </CardBody>
           </div>
@@ -107,9 +140,13 @@ const Dashboard = () => {
                   <div className="h-10 w-10 animate-spin rounded-full border-8 border-gray-300 border-t-purple-500" />
                 </div>
               ) : (
-                <Typography variant="h3" className="text-primary">
+                <div className="flex flex-row items-center justify-center gap-2">
+                  <Typography variant="h3" className={`${riderTrend?"text-primary":"text-[#C70381]"}`}>
                   {activeRiders}
                 </Typography>
+                {riderTrend?<TrendingUp className="text-primary"/>:<TrendingDown className="text-[#C70381]"/>}
+                </div>
+            
               )}
             </CardBody>
           </div>
@@ -128,6 +165,7 @@ const Dashboard = () => {
         </div>
       </div>
     </div>
+    </Card>
   );
 };
 
