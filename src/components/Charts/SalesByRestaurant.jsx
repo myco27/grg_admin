862f62d1e691b7ec137af9adceb0c9b4fbb07a26
DonMonsieur <124369678+DonMonsieur@users.noreply.github.com>
2025-04-04 12:05:07 +0800

    # sales by restaurant monthly

--- src/components/Charts/SalesByRestaurant.jsx
+++ src/components/Charts/SalesByRestaurant.jsx
@@ -1,113 +1,245 @@
+import React, { useEffect, useState } from "react";
 import {
   Card,
   CardBody,
-  CardFooter,
   CardHeader,
   Typography,
 } from "@material-tailwind/react";
 import Chart from "react-apexcharts";
+import axiosClient from "../../axiosClient";
 
 const chartConfig = {
-    type: "bar",
-    height: 250,
-    series: [
+  type: "bar",
+  height: 250,
+  series: [
+    {
+      name: "Actual",
+      data: [
         {
-            name: "Actual",
-            data: [
-                { x: "Bangsar", y: 10, goals: [{ name: "Expected", value: 10, strokeWidth: 15, strokeColor: "#612B9B4D"
- }] },
-                { x: "KLCC", y: 44, goals: [{ name: "Expected", value: 44, strokeWidth: 15, strokeColor: "#612B9B4D"
- }] },
-                { x: "Changkat", y: 54, goals: [{ name: "Expected", value: 54, strokeWidth: 15, strokeColor: "#612B9B4D"
- }] },
-                { x: "Damansara", y: 66, goals: [{ name: "Expected", value: 66, strokeWidth: 15, strokeColor: "#612B9B4D"
- }] },
-                { x: "Ramlee", y: 81, goals: [{ name: "Expected", value: 81, strokeWidth: 15, strokeColor: "#612B9B4D"
- }] },
-                { x: "Melaka", y: 67, goals: [{ name: "Expected", value: 67, strokeWidth: 15, strokeColor: "#612B9B4D"
- }] },
-                { x: "Sunway", y: 67, goals: [{ name: "Expected", value: 67, strokeWidth: 15, strokeColor: "#612B9B4D"
- }] },
-                { x: "Queens bay", y: 67, goals: [{ name: "Expected", value: 67, strokeWidth: 15, strokeColor: "#612B9B4D"
- }] },
-                { x: "Auto City", y: 67, goals: [{ name: "Expected", value: 67, strokeWidth: 15, strokeColor: "#612B9B4D"
- }] },
-                { x: "Chulia", y: 81, goals: [{ name: "Expected", value: 81, strokeWidth: 15, strokeColor: "#612B9B4D"
- }] },
-                { x: "Gurney", y: 92, goals: [{ name: "Expected", value: 92, strokeWidth: 15, strokeColor: "#612B9B4D"
- }] },
-                { x: "Batu", y: 48, goals: [{ name: "Expected", value: 48, strokeWidth: 15, strokeColor: "#612B9B4D"
- }] },
-            ],
-        },
-    ],
-    options: {
-        chart: {
-            height: 1000,
-            type: "bar",
-        },
-        plotOptions: {
-            bar: {
-                columnWidth: "30",
-                horizontal: false,
+          x: "Bangsar",
+          y: 10,
+          goals: [
+            {
+              name: "Expected",
+              value: 10,
+              strokeWidth: 15,
+              strokeColor: "#612B9B4D",
             },
+          ],
         },
-        colors: ["#612B9B4D"],
-        xaxis: {
-            labels: {
-                style: {
-                    fontSize: "6px", 
-                    fontWeight: "bold",
-                },
+        {
+          x: "KLCC",
+          y: 44,
+          goals: [
+            {
+              name: "Expected",
+              value: 44,
+              strokeWidth: 15,
+              strokeColor: "#612B9B4D",
             },
+          ],
         },
-        yaxis: {
-            labels: {
-                show:false,
-                style: {
-                    fontSize: "14px", 
-                    fontWeight: "bold",
-                },
+        {
+          x: "Changkat",
+          y: 54,
+          goals: [
+            {
+              name: "Expected",
+              value: 54,
+              strokeWidth: 15,
+              strokeColor: "#612B9B4D",
             },
+          ],
         },
-        legend: {
-            labels: {
-                colors: "#000",
-                style: {
-                    fontSize: "14px", 
-                },
+        {
+          x: "Damansara",
+          y: 66,
+          goals: [
+            {
+              name: "Expected",
+              value: 66,
+              strokeWidth: 15,
+              strokeColor: "#612B9B4D",
             },
+          ],
         },
-        tooltip: {
-            style: {
-                fontSize: "14px", 
+        {
+          x: "Ramlee",
+          y: 81,
+          goals: [
+            {
+              name: "Expected",
+              value: 81,
+              strokeWidth: 15,
+              strokeColor: "#612B9B4D",
             },
+          ],
         },
-        dataLabels: {
-            enabled:false,
-            style: {
-                fontSize: "12px", // Change data label font size
-                fontWeight: "bold",
+        {
+          x: "Melaka",
+          y: 67,
+          goals: [
+            {
+              name: "Expected",
+              value: 67,
+              strokeWidth: 15,
+              strokeColor: "#612B9B4D",
             },
+          ],
         },
+        {
+          x: "Sunway",
+          y: 67,
+          goals: [
+            {
+              name: "Expected",
+              value: 67,
+              strokeWidth: 15,
+              strokeColor: "#612B9B4D",
+            },
+          ],
+        },
+        {
+          x: "Queens bay",
+          y: 67,
+          goals: [
+            {
+              name: "Expected",
+              value: 67,
+              strokeWidth: 15,
+              strokeColor: "#612B9B4D",
+            },
+          ],
+        },
+        {
+          x: "Auto City",
+          y: 67,
+          goals: [
+            {
+              name: "Expected",
+              value: 67,
+              strokeWidth: 15,
+              strokeColor: "#612B9B4D",
+            },
+          ],
+        },
+        {
+          x: "Chulia",
+          y: 81,
+          goals: [
+            {
+              name: "Expected",
+              value: 81,
+              strokeWidth: 15,
+              strokeColor: "#612B9B4D",
+            },
+          ],
+        },
+        {
+          x: "Gurney",
+          y: 92,
+          goals: [
+            {
+              name: "Expected",
+              value: 92,
+              strokeWidth: 15,
+              strokeColor: "#612B9B4D",
+            },
+          ],
+        },
+        {
+          x: "Batu",
+          y: 48,
+          goals: [
+            {
+              name: "Expected",
+              value: 48,
+              strokeWidth: 15,
+              strokeColor: "#612B9B4D",
+            },
+          ],
+        },
+      ],
     },
+  ],
+  options: {
+    chart: { height: 1000, type: "bar" },
+    plotOptions: {
+      bar: { columnWidth: "30", horizontal: false },
+    },
+    colors: ["#612B9B4D"],
+    xaxis: {
+      labels: { style: { fontSize: "6px", fontWeight: "bold" } },
+    },
+    yaxis: {
+      labels: {
+        show: false,
+        style: { fontSize: "14px", fontWeight: "bold" },
+      },
+    },
+    legend: {
+      labels: {
+        colors: "#000",
+        style: { fontSize: "14px" },
+      },
+    },
+    tooltip: {
+      style: { fontSize: "14px" },
+    },
+    dataLabels: {
+      enabled: false,
+      style: { fontSize: "12px", fontWeight: "bold" },
+    },
+  },
 };
 
+const SalesByRestaurant = () => {
+  const [loading, setLoading] = useState(false);
+  const [totalMonthlySales, setTotalMonthlySales] = useState([]);
 
-export default function SalesByRestaurant() {
+  const fetchSalesByRestaurants = async () => {
+    try {
+      setLoading(true);
+      const response = await axiosClient.get(
+        "/admin/dashboard/sales-by-restaurant"
+      );
+
+      if (response.status === 200) {
+        console.log(response.data);
+        setTotalMonthlySales(response.data.totalData.totalNetSales);
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchSalesByRestaurants();
+  }, []);
+
   return (
     <Card>
       <CardHeader
         floated={false}
         shadow={false}
         color="transparent"
-        className="flex flex-col gap-4 rounded-none md:flex-row md:items-center"
+        className="flex flex-col gap-2 rounded-none"
       >
-        <Typography color='black' variant="h5">Sales by Restaurant</Typography>
+        <Typography color="black" variant="h5">
+          Sales by Restaurant
+        </Typography>
+        <Typography>
+          <span className="block">{totalMonthlySales}</span>
+          <span className="text-gray-500">Total Sales (Monthly)</span>
+        </Typography>
       </CardHeader>
       <CardBody className="px-2 pb-0">
         <Chart {...chartConfig} />
       </CardBody>
     </Card>
   );
-}
+};
+
+export default SalesByRestaurant;
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -9,36 +9,38 @@ import axiosClient from "../../axiosClient";
 import OrderRate from "../../components/Charts/OrderRate";
 import SalesByRestaurant from "../../components/Charts/SalesByRestaurant";
 import SalesByCentral from "../../components/Charts/SalesByCentral";
-import { Bike, Building2, Hotel, TrendingDown, TrendingUp, UserRound } from "lucide-react";
+import {
+  Bike,
+  Building2,
+  Hotel,
+  TrendingDown,
+  TrendingUp,
+  UserRound,
+} from "lucide-react";
 
 const Dashboard = () => {
   const [activeCustomers, setActiveCustomers] = useState(0);
   const [activeRiders, setActiveRiders] = useState(0);
   const [totalCentral, setTotalCentral] = useState(0);
   const [totalRestaurants, setTotalRestaurants] = useState(0);
-  const [customerTrend, setCustomerTrend] = useState(true)
-  const [riderTrend, setRiderTrend] = useState(false)
+  const [customerTrend, setCustomerTrend] = useState(true);
+  const [riderTrend, setRiderTrend] = useState(false);
   const [loading, setLoading] = useState(false);
 
-
-
   const fetchStoreMontly = async () => {
-    try{
-      setLoading(true)
+    try {
+      setLoading(true);
       const response = await axiosClient.get("/admin/dashboard/card-data");
-      
-      if (response=== 200){
-        
+
+      if (response === 200) {
       }
-      
-    }catch(e){console.log(e.error)}
-    finally{setLoading(false)}
+    } catch (e) {
+      console.log(e.error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    
-  }
-
-
-  
   const fetchDashboardCardData = async () => {
     setLoading(true);
     try {
@@ -50,8 +52,6 @@ const Dashboard = () => {
         setTotalCentral(response.data.data.totalNumberOfCentral);
         setTotalRestaurants(response.data.data.totalNumberOfRestaurant);
       }
-
-      console.log(response.data)
     } catch (error) {
       console.error("Error fetching dashboard card data:", error);
     } finally {
@@ -59,118 +59,127 @@ const Dashboard = () => {
     }
   };
 
-
-
-
- 
-  
   useEffect(() => {
     fetchDashboardCardData();
   }, []);
 
-
-  
-
   return (
     <Card className="rounded-none shadow-none">
-    <div className="m-5 flex flex-col justify-center gap-5">
-      <div id="row1" className="flex flex-grow flex-col justify-evenly gap-1 sm:flex-row">
-        <Card className="item-center flex max-w-[370px] justify-center">
-        <div id="div1" className="grid grid-cols-2 grid-rows-2 gap-2 px-5">
-          <div className="flex items-center justify-center rounded-md border">
-            <CardBody className="text-center">
-              <Typography variant="h5" className="mb-2 flex flex-row gap-1" >
-              <Building2 className="rounded"/> Central
-              </Typography>
-              {loading ? (
-                <div className="flex items-center justify-center">
-                  <div className="h-10 w-10 animate-spin rounded-full border-8 border-gray-300 border-t-purple-500" />
-                </div>
-              ) : (
-                <Typography variant="h3" className="text-primary" >
-        
-                {totalCentral}
-                </Typography>
-              )}
-            </CardBody>
-          </div>
-          <div className="flex min-w-fit items-center justify-center rounded-md border">
-            <CardBody className="text-center">
-              <Typography variant="h5" className="mb-2 flex flex-row">
-                <UserRound/>Customer
-              </Typography>
-              {loading ? (
-                <div className="flex items-center justify-center">
-                  <div className="h-10 w-10 animate-spin rounded-full border-t-purple-500" />
-                </div>
-              ) : (
-                <div className="flex flex-row items-center justify-center gap-2">
-                <Typography variant="h3" className={`${customerTrend?"text-primary":"text-[#C70381]"}`}>
-                  {activeCustomers}
-                </Typography>
-                {customerTrend?<TrendingUp className="text-primary"/>:<TrendingDown className="text-[#C70381]"/>}
-                </div>
-              )}
-            </CardBody>
-          </div>
-          <div className="flex items-center justify-center rounded-md border">
-            <CardBody className="text-center">
-              <Typography variant="h5" className="mb-2 flex flex-row gap-1">
-                <Hotel/>Restaurant
-              </Typography>
-              
-              {loading ? (
-                <div className="flex items-center justify-center">
-                  <div className="h-10 w-10 animate-spin rounded-full" />
-                </div>
-              ) : (
-                <Typography variant="h3" className="text-primary">
-                  {totalCentral}
-                </Typography>
-              )}
-            </CardBody>
-          </div>
-          <div className="flex min-w-fit items-center justify-center rounded-md border">
-            <CardBody className="text-center">
-              <Typography variant="h5" className="mb-2 flex flex-row gap-1">
-                <Bike/> Riders
-              </Typography>
-              {loading ? (
-                <div className="flex items-center justify-center">
-                  <div className="h-10 w-10 animate-spin rounded-full border-8 border-gray-300 border-t-purple-500" />
-                </div>
-              ) : (
-                <div className="flex flex-row items-center justify-center gap-2">
-                  <Typography variant="h3" className={`${riderTrend?"text-primary":"text-[#C70381]"}`}>
-                  {activeRiders}
-                </Typography>
-                {riderTrend?<TrendingUp className="text-primary"/>:<TrendingDown className="text-[#C70381]"/>}
-                </div>
-            
-              )}
-            </CardBody>
+      <div className="m-5 flex flex-col justify-center gap-5">
+        <div
+          id="row1"
+          className="flex flex-grow flex-col justify-evenly gap-1 sm:flex-row"
+        >
+          <Card className="item-center flex max-w-[370px] justify-center">
+            <div id="div1" className="grid grid-cols-2 grid-rows-2 gap-2 px-5">
+              <div className="flex items-center justify-center rounded-md border">
+                <CardBody className="text-center">
+                  <Typography variant="h5" className="mb-2 flex flex-row gap-1">
+                    <Building2 className="rounded" /> Central
+                  </Typography>
+                  {loading ? (
+                    <div className="flex items-center justify-center">
+                      <div className="h-10 w-10 animate-spin rounded-full border-8 border-gray-300 border-t-purple-500" />
+                    </div>
+                  ) : (
+                    <Typography variant="h3" className="text-primary">
+                      {totalCentral}
+                    </Typography>
+                  )}
+                </CardBody>
+              </div>
+              <div className="flex min-w-fit items-center justify-center rounded-md border">
+                <CardBody className="text-center">
+                  <Typography variant="h5" className="mb-2 flex flex-row">
+                    <UserRound />
+                    Customer
+                  </Typography>
+                  {loading ? (
+                    <div className="flex items-center justify-center">
+                      <div className="h-10 w-10 animate-spin rounded-full border-t-purple-500" />
+                    </div>
+                  ) : (
+                    <div className="flex flex-row items-center justify-center gap-2">
+                      <Typography
+                        variant="h3"
+                        className={`${
+                          customerTrend ? "text-primary" : "text-[#C70381]"
+                        }`}
+                      >
+                        {activeCustomers}
+                      </Typography>
+                      {customerTrend ? (
+                        <TrendingUp className="text-primary" />
+                      ) : (
+                        <TrendingDown className="text-[#C70381]" />
+                      )}
+                    </div>
+                  )}
+                </CardBody>
+              </div>
+              <div className="flex items-center justify-center rounded-md border">
+                <CardBody className="text-center">
+                  <Typography variant="h5" className="mb-2 flex flex-row gap-1">
+                    <Hotel />
+                    Restaurant
+                  </Typography>
+
+                  {loading ? (
+                    <div className="flex items-center justify-center">
+                      <div className="h-10 w-10 animate-spin rounded-full" />
+                    </div>
+                  ) : (
+                    <Typography variant="h3" className="text-primary">
+                      {totalCentral}
+                    </Typography>
+                  )}
+                </CardBody>
+              </div>
+              <div className="flex min-w-fit items-center justify-center rounded-md border">
+                <CardBody className="text-center">
+                  <Typography variant="h5" className="mb-2 flex flex-row gap-1">
+                    <Bike /> Riders
+                  </Typography>
+                  {loading ? (
+                    <div className="flex items-center justify-center">
+                      <div className="h-10 w-10 animate-spin rounded-full border-8 border-gray-300 border-t-purple-500" />
+                    </div>
+                  ) : (
+                    <div className="flex flex-row items-center justify-center gap-2">
+                      <Typography
+                        variant="h3"
+                        className={`${
+                          riderTrend ? "text-primary" : "text-[#C70381]"
+                        }`}
+                      >
+                        {activeRiders}
+                      </Typography>
+                      {riderTrend ? (
+                        <TrendingUp className="text-primary" />
+                      ) : (
+                        <TrendingDown className="text-[#C70381]" />
+                      )}
+                    </div>
+                  )}
+                </CardBody>
+              </div>
+            </div>
+          </Card>
+          <div className="flex-grow">
+            <SalesByRestaurant />
           </div>
         </div>
-        </Card>
-        <div className="flex-grow">
-          <SalesByRestaurant/>
+        <div className="flex flex-col gap-1 sm:flex-row">
+          <div className="flex">
+            <SalesByCentral />
+          </div>
+          <div className="flex-grow">
+            <OrderRate></OrderRate>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col gap-1 sm:flex-row">
-        <div className="flex">
-            <SalesByCentral/>
-        </div>
-        <div className="flex-grow">
-          <OrderRate></OrderRate>
-        </div>
-      </div>
-    </div>
     </Card>
   );
 };
 
-{
-  /*
-   */
-}
 export default Dashboard;
